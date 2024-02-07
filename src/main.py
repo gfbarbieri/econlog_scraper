4c4e3dd4752966b10676e500a9c2182ce1fc0811
Greg Barbieri <gfbarbieri@gmail.com>
2024-02-07 12:08:08 -0500

    Main is a full-featured example.

--- src/main.py
+++ src/main.py
@@ -1,19 +1,12 @@
-import argparse
 from scraper import EconlogScraper
 from utils import text_utils
 from utils import data_io
 
 def main():
-    # Parsing arguments.
-    print("Parsing arguments.")
-    parser = argparse.ArgumentParser()
-    parser.add_argument('author', type=str)
-    args = parser.parse_args()
-
     # Initialized scraper using defaults.
-    print(f"Initialize scraper with author {args.author}")
-    els = EconlogScraper(author=args.author)
-    print(f"Scraper initialized with author {els.author}")
+    print(f"Initialize scraper with default author.")
+    els = EconlogScraper()
+    print(f"Scraper initialized with author {els.author}.")
 
     # Request all of the article containers for the author. Returns a list
     # of HTML containers.
@@ -24,44 +17,49 @@ def main():
     # Request the metadata from all article containers. Returns a list of
     # dictionaries.
     print("Extracting metadata for each container.")
-    metadata = els.extract_article_metadata(container_list=containers)
+    metadata = (
+        [
+            els.extract_article_metadata(article_container=container)
+            for container in containers
+        ]
+    )
     print(f"Metadata extracted from {len(metadata)} containers.")
 
     # For each article: (1) request the article's content, including its
     # text and labels. From the text, (2) extract the raw text from HTML
     # tags and (3) any embedded urls from HTML tags. From the raw text, (4)
     # perform a word count and (5) get a word list.
-    print("Requesting article contents and performing text frequency.")
+    print("Requesting article contents and extracting features.")
 
     for indx, article in enumerate(metadata):
-        print("{indx}: Starting article.")
+        print(f"{indx}: Starting article.")
 
         # 1. Request the article's content.
-        print("{indx}: Requesting article content.")
+        print(f"{indx}: Requesting article content.")
         p_tags, topics = els.request_article_content(url=article['url'])
 
         # 2. Extract raw text from the HTML <p> tags.
-        print("{indx}: Extracting raw text.")
-        text = text_utils.extract_article_text(article=p_tags)
+        print(f"{indx}: Extracting raw text.")
+        text = els.extract_article_text(article_content=p_tags)
 
         # 3. Extract embedded URLs from the HTML <a> tags. This is required
         # for the subsequent network analysis on embedded articles.
-        print("{indx}: Extracting embedded URLs.")
-        embedded_urls = text_utils.extract_embedded_urls(article=p_tags)
+        print(f"{indx}: Extracting embedded URLs.")
+        embedded_urls = els.extract_embedded_urls(article_content=p_tags)
 
         # 4. & 5. Count total and individual words.
-        print("{indx}: Performing word counts.")
+        print(f"{indx}: Performing word counts.")
         word_count, word_freq = text_utils.word_counter(document=text)
 
         # Add to dictionary.
-        print("{indx}: Adding results to metadata dictionary.")
+        print(f"{indx}: Adding results to metadata dictionary.")
         article['text'] = text
         article['topics'] = topics
         article['embedded_urls'] = embedded_urls
         article['word_count'] = word_count
         article['word_freq'] = word_freq
 
-        print("{indx}: Article completed.")
+        print(f"{indx}: Article completed.")
 
     print("Completed all articles.")
 
--- src/utils/data_io.py
+++ src/utils/data_io.py
@@ -37,7 +37,7 @@ def read_file(file_name):
     except Exception as e:
         print(f"An exception occured: {e}")
         return None
-    
+
 def write_file(data, file_name):
     """
     Writes data to a file based on its extension. Supported file types:
@@ -50,14 +50,20 @@ def write_file(data, file_name):
         extn = extn.lower()
 
         # Write the data to the file path.
-        file_path = os.path.join(os.path.dirname(__file__), '..', '..', 'data', file_name)
+        file_path = os.path.join('../data', file_name)
         
         if extn == '.pkl':
-            with open(file_path, 'wb') as f:
-                pickle.dump(data, file_path)
+            try:
+                with open(file_path, 'wb') as f:
+                    pickle.dump(data, f)
+            except RecursionError as e:
+                str_data = [str(element) for element in data]
+
+                with open(file_path, 'wb') as f:
+                    pickle.dump(str_data, f)
         elif extn == '.json':
             with open(file_path, 'w', encoding='utf-8') as f:
-                json.dump(data, file_path, ensure_ascii=False, indent=4)
+                json.dump(data, f, ensure_ascii=False, indent=4)
         else:
             print(f"Unsupported file type: {extn}")
     except IOError as e:
